001adfe28d58af93f98fea4864ac45096ee12b23
Ranulph <ranulph@mailfence.run>
2024-07-02 13:37:05 +1000

    Updated to Next 14

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import './globals.css';
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import { Inter } from 'next/font/google';
 import { ThemeProvider } from "@/components/theme-provider";
 import { Gradient } from "@/components/ui/gradient";
@@ -14,18 +14,6 @@ export const metadata: Metadata = {
   applicationName: 'Units',
   authors: [{ name: 'Ranulph', url: 'https://ranulph.run' }],
   keywords: "Units, unit converter, unit convertor, unit conversion, currency converter, currency conversion",
-  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#1A1A1A" }, { media: "(prefers-color-scheme: light)", color: "#FAFAFA" }],
-  colorScheme: 'dark light',
-  viewport: {
-    width: 'device-width',
-    height: 'device-height',
-    interactiveWidget: 'resizes-visual',
-    initialScale: 1.0,
-    minimumScale: 1.0,
-    maximumScale: 1.0,
-    userScalable: false,
-    viewportFit: 'cover'
-  },
   manifest: "https://units.run/site.webmanifest",
   icons: [{ rel: "icon", url: "https://units.run/android/android-launchericon-512-512.png" }, { rel: "apple-touch-icon", url: "https://units.run/ios/512.png" }],
   openGraph: {
@@ -37,6 +25,19 @@ export const metadata: Metadata = {
   },
   appleWebApp: { capable: true, title: "Units", statusBarStyle: "black-translucent" }
 };
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  height: 'device-height',
+  interactiveWidget: 'resizes-visual',
+  initialScale: 1.0,
+  minimumScale: 1.0,
+  maximumScale: 1.0,
+  userScalable: false,
+  viewportFit: 'cover',
+  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#1A1A1A" }, { media: "(prefers-color-scheme: light)", color: "#FAFAFA" }],
+  colorScheme: 'dark light',
+}
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
 
